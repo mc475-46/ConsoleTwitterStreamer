82778a4e25ae76411547120fc8f3dd67724c3101
Motoki Nagano <motoki.n.310@gmail.com>
2016-07-13 16:44:37 +0900

    sleepする間隔を変更

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -66,7 +66,7 @@ namespace ConsoleApplication1
 
             while (true)
             {
-                Thread.Sleep(100000000);
+                Thread.Sleep(100);
             }
             stream.Dispose();
         }
